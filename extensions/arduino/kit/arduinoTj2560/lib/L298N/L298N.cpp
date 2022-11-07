1d365455660ff3e21d4d8104f7f0890f0e2890f9
HighBrightly <794858095@qq.com>
2022-11-07 14:06:44 +0800

    1.fixbug: the motor caused the system to restart!

--- extensions/arduino/kit/arduinoTj2560/lib/L298N/L298N.cpp
+++ extensions/arduino/kit/arduinoTj2560/lib/L298N/L298N.cpp
@@ -13,6 +13,40 @@ L298N::L298N(uint8_t pinEnable, uint8_t pinIN1, uint8_t pinIN2)
 
 void L298N::run(int direction, int speed)
 {
+  //__________修复主板重启电机bug___________________________________________________
+  if(direction == 0){	 //第一次赋值
+	_start_speed = speed;
+	_start_direction = direction;
+}
+ if (_start_direction != direction)  //前后或者后前
+ {
+   if (_start_speed > 0 && speed > 0) { //正正
+      if (_start_speed + speed > 400) {
+        this->stop();
+        delay(50);
+      }
+    } else if (_start_speed < 0 && speed < 0) { //负负
+      if (-_start_speed + speed > 400) {
+        this->stop();
+        delay(50);
+      }
+    }
+  }
+  else {                                //前前或者后后
+    if (_start_speed > 0 && speed < 0) {  //速度参数不同；正负
+      if (_start_speed - speed > 400) {
+        this->stop();
+        delay(50);
+      }
+    } else if (_start_speed < 0 && speed > 0) {  //速度参数不同；负正
+      if (speed - _start_speed > 400) {
+        this->stop();
+        delay(50);
+      }
+    }
+  }	
+  //____________________________________________________________
+  
   if (speed <= 0)
   {
     speed = -speed;
