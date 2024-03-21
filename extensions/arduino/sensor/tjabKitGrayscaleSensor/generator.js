23e2eb624ba35da9475515b46647e1500a09beaa
Bright <50515279+HighBrightly@users.noreply.github.com>
2024-03-21 11:07:47 +0800

    Update generator.js
    FIX BUG: GrayscaleSensor ERRO!

--- extensions/arduino/sensor/tjabKitGrayscaleSensor/generator.js
+++ extensions/arduino/sensor/tjabKitGrayscaleSensor/generator.js
@@ -8,14 +8,14 @@ function addGenerator (Blockly) {
         const sta = block.getFieldValue('STA');
 
         Blockly.Arduino.setups_[`tjabKitGrayscaleSensor_read${port}`] = `pinMode(${port}, INPUT_PULLUP);`;
-        Blockly.Arduino.includes_.arduinoTj2560Ext_onBoardDriver_io = `#include <io_tj2560.h>`;
+        //Blockly.Arduino.includes_.arduinoTj2560Ext_onBoardDriver_io = `#include <io_tj2560.h>`;
 
         return [`(digitalRead(${port}) == ${sta})`, Blockly.Arduino.ORDER_ATOMIC];
     };
     Blockly.Arduino.tjabKitGrayscaleSensor_AnalogRead = function (block) {
         const port = block.getFieldValue('PORT');
         Blockly.Arduino.setups_[`tjabKitGrayscaleSensor_AnalogRead${port}`] = `pinMode(${port}, INPUT);`;
-        Blockly.Arduino.includes_.arduinoTj2560Ext_onBoardDriver_io = `#include <io_tj2560.h>`;
+        //Blockly.Arduino.includes_.arduinoTj2560Ext_onBoardDriver_io = `#include <io_tj2560.h>`;
 
         return [`map(constrain(analogRead(${port}), 0, 850), 0, 850, 1023, 0)`, Blockly.Arduino.ORDER_ATOMIC];
     };
