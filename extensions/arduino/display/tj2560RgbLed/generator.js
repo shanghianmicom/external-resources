a3bc3740e29f5ce2baff07e976d81906c5cb6cac
HighBrightly <794858095@qq.com>
2024-03-27 16:19:33 +0800

    fixbug

--- extensions/arduino/display/tj2560RgbLed/generator.js
+++ extensions/arduino/display/tj2560RgbLed/generator.js
@@ -8,6 +8,7 @@ function addGenerator (Blockly) {
         tj2560RgbLed_port = port;
         const colour = Blockly.Arduino.valueToCode(block, 'COLOR', Blockly.Arduino.ORDER_ATOMIC).replace('#', '0x');
         Blockly.Arduino.includes_.tj2560RgbLed_setPixelColor = `#include <Adafruit_NeoPixel.h>`;
+        Blockly.Arduino.includes_.arduinoTj2560Ext_onBoardDriver_io = `#include <io_tj2560.h>`;
         Blockly.Arduino.definitions_[`tj2560RgbLed_setPixelColor_${port}`] = `Adafruit_NeoPixel ledStrip_${port}(1, pinMap[${port}][S5], NEO_GRB + NEO_KHZ800);`;
         Blockly.Arduino.setups_[`tj2560RgbLed_setPixelColor_${port}`] = `ledStrip_${port}.begin();`;
 
