66df2125e16177d3ec0db54696a0ab634bb9d534
JiazhengLiuUTD <liujiazheng94@gmail.com>
2024-07-19 12:15:37 +0800

    New robotic arm and updated openmv are available now.

--- extensions/arduino/actuator/tj2560RoboticArm/index.js
+++ extensions/arduino/actuator/tj2560RoboticArm/index.js
@@ -7,7 +7,7 @@ const tj2560RoboticArm = formatMessage => ({
     version: '1.0.0',
     supportDevice: ['arduinoTj2560'],
     author: 'Jiazheng Liu',
-    iconURL: `asset/tj2560RoboticArm.jpg`,
+    iconURL: `asset/tj2560RoboticArm.png`,
     description: formatMessage({
         id: 'tj2560RoboticArm.description',
         default: 'Robotic Arm.'
